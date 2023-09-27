38b4c0c4961068f5e5cb7456b5ae80a3a2699593
KorolYurii <sminser111@gmail.com>
2023-09-27 20:54:44 +0300

    hw - 21

--- 21 Об'єкти/JS/main.dev.js
+++ 21 Об'єкти/JS/main.dev.js
@@ -22,37 +22,38 @@ var car = {
     return this.drivers.includes(driverName);
   },
   consumption: function consumption() {
-    var fix = this.distance / 100 * this.fuel;
-    console.log(fix);
+    var fixFuel = this.distance / 100 * this.fuel;
+    console.log("\u0432\u0441\u044C\u043E\u0433\u043E \u043F\u043E\u0442\u0440\u0456\u0431\u043D\u043E ".concat(fixFuel, "\u043B. \u043F\u0430\u043B\u044C\u043D\u043E\u0433\u043E"));
   },
   calcTime: function calcTime() {
-    var hours = this.distance / this.averSreed; // console.log(hours);
+    var hours = this.distance / this.averSreed;
 
-    if (hours >= 4) {
-      console.log(hours + 1);
-    } else if (hours >= 9) {
-      console.log(hours + 2);
+    if (hours >= 12) {
+      console.log("\u0447\u0430\u0441 \u0437\u0430 \u044F\u043A\u0438\u0439 \u0432\u0438 \u043F\u043E\u0434\u043E\u043B\u0430\u0454\u0442\u0435 \u0432\u0456\u0434\u0441\u0442\u0430\u043D\u044C \u0431\u0443\u0434\u0435: ".concat(hours + 3, " \u0433\u043E\u0434."));
+    } else if (hours >= 8) {
+      console.log("\u0447\u0430\u0441 \u0437\u0430 \u044F\u043A\u0438\u0439 \u0432\u0438 \u043F\u043E\u0434\u043E\u043B\u0430\u0454\u0442\u0435 \u0432\u0456\u0434\u0441\u0442\u0430\u043D\u044C \u0431\u0443\u0434\u0435: ".concat(hours + 2, " \u0433\u043E\u0434."));
+    } else if (hours >= 4) {
+      console.log("\u0447\u0430\u0441 \u0437\u0430 \u044F\u043A\u0438\u0439 \u0432\u0438 \u043F\u043E\u0434\u043E\u043B\u0430\u0454\u0442\u0435 \u0432\u0456\u0434\u0441\u0442\u0430\u043D\u044C \u0431\u0443\u0434\u0435: ".concat(hours + 1, " \u0433\u043E\u0434."));
     }
   }
 };
 car.addDriver('Dima');
 console.log(car.checkDriver('Victor'));
 car.showInfo();
-car.distance = 2000;
+car.distance = 3000;
 car.consumption();
 car.calcTime(); //normal
 
-var Time = {
+var time = {
   hours: 0,
   minutes: 0,
   seconds: 0,
   displayTime: function displayTime() {
-    var hoursStr = String(this.hours).padStart(2, "0");
-    var minutesStr = String(this.minutes).padStart(2, "0");
-    var secondsStr = String(this.seconds).padStart(2, "0");
+    var hoursStr = this.hours < 10 ? "0" + this.hours : this.hours.toString();
+    var minutesStr = this.minutes < 10 ? "0" + this.minutes : this.minutes.toString();
+    var secondsStr = this.seconds < 10 ? "0" + this.seconds : this.seconds.toString();
     console.log("".concat(hoursStr, ":").concat(minutesStr, ":").concat(secondsStr));
   },
-  // Функция для изменения времени на переданное количество секунд
   addSeconds: function addSeconds(seconds) {
     var totalSeconds = this.hours * 3600 + this.minutes * 60 + this.seconds + seconds;
     this.hours = Math.floor(totalSeconds / 3600) % 24;
@@ -60,122 +61,68 @@ var Time = {
     this.minutes = Math.floor(remainingSeconds / 60);
     this.seconds = remainingSeconds % 60;
   },
-  // Функция для изменения времени на переданное количество минут
   addMinutes: function addMinutes(minutes) {
     this.addSeconds(minutes * 60);
   },
-  // Функция для изменения времени на переданное количество часов
   addHours: function addHours(hours) {
     this.addSeconds(hours * 3600);
   }
-}; // Пример использования объекта и функций
+};
+var currentTime = Object.create(time); // currentTime.hours = 20;
+// currentTime.minutes = 59;
+// currentTime.seconds = 45;
 
-var currentTime = Object.create(Time);
-currentTime.hours = 20;
-currentTime.minutes = 59;
-currentTime.seconds = 45;
-currentTime.displayTime(); // Выведет "20:59:45"
+currentTime.displayTime();
+currentTime.addSeconds(101);
+currentTime.displayTime();
+currentTime.addMinutes(250);
+currentTime.displayTime();
+currentTime.addHours(14);
+currentTime.displayTime(); //max
 
-currentTime.addSeconds(30);
-currentTime.displayTime(); // Выведет "21:00:15"
+var fraction = {
+  nominator: 2,
+  denominator: 3,
+  showValue: function showValue() {
+    console.log("Value is ".concat(this.nominator, "/").concat(this.denominator));
+  },
+  findGCD: function findGCD(a, b) {
+    if (a == 0) {
+      return b;
+    }
 
-currentTime.addMinutes(75);
-currentTime.displayTime(); // Выведет "22:15:15"
+    return this.findGCD(b % a, a);
+  },
+  findLocalNominator: function findLocalNominator(n, d) {
+    return n * d;
+  },
+  add: function add(n, d) {
+    var localDenominator = 1;
 
-currentTime.addHours(2);
-currentTime.displayTime(); // Выведет "00:15:15" (следующий день)
-// const time = {
-//   hours: 0,
-//   minute: 0,
-//   seconds: 0,
-// }
-// function enterTime() {  
-//   let now = new Date();
-//   console.log( now );
-// }
-// function calcTimeSec() {
-//   let date = new Date();
-//   date.setSeconds(date.getSeconds() + 300);
-//   alert( date );
-// }
-// function calcTimeMin() {
-//   let date = new Date();
-//   date.setMinutes(date.getMinutes() + 300);
-//   alert( date );
-// }
-// function calcTimeHours() {
-//   let date = new Date();
-//   date.setHours(date.getHours() + 300);
-//   alert( date );
-// }
-//max
-
-var Fraction = {
-  numerator: 0,
-  denominator: 1,
-  // Метод для создания новой дроби
-  create: function create(numerator, denominator) {
-    var fraction = Object.create(this);
-    fraction.numerator = numerator;
-    fraction.denominator = denominator;
-    return fraction;
-  },
-  // Метод для сложения двух дробей
-  add: function add(otherFraction) {
-    var commonDenominator = this.denominator * otherFraction.denominator;
-    var newNumerator = this.numerator * otherFraction.denominator + otherFraction.numerator * this.denominator;
-    return Fraction.create(newNumerator, commonDenominator);
-  },
-  // Метод для вычитания двух дробей
-  subtract: function subtract(otherFraction) {
-    var commonDenominator = this.denominator * otherFraction.denominator;
-    var newNumerator = this.numerator * otherFraction.denominator - otherFraction.numerator * this.denominator;
-    return Fraction.create(newNumerator, commonDenominator);
-  },
-  // Метод для умножения двух дробей
-  multiply: function multiply(otherFraction) {
-    var newNumerator = this.numerator * otherFraction.numerator;
-    var newDenominator = this.denominator * otherFraction.denominator;
-    return Fraction.create(newNumerator, newDenominator);
-  },
-  // Метод для деления двух дробей
-  divide: function divide(otherFraction) {
-    var newNumerator = this.numerator * otherFraction.denominator;
-    var newDenominator = this.denominator * otherFraction.numerator;
-    return Fraction.create(newNumerator, newDenominator);
-  },
-  // Метод для сокращения дроби
-  reduce: function reduce() {
-    var gcd = this.gcd(this.numerator, this.denominator);
-    return Fraction.create(this.numerator / gcd, this.denominator / gcd);
-  },
-  // Метод для нахождения наибольшего общего делителя (Алгоритм Евклида)
-  gcd: function gcd(a, b) {
-    if (b === 0) {
-      return a;
+    if (this.denominator === d) {
+      localDenominator = this.denominator;
     } else {
-      return this.gcd(b, a % b);
+      localDenominator = this.denominator * d;
+      this.nominator = this.nominator * (localDenominator / this.denominator);
+      this.denominator = localDenominator;
+      n = n * (localDenominator / d);
+      d = localDenominator;
+      var addingResultNominator = this.nominator + n;
+      var gcd = this.findGCD(addingResultNominator, d);
+      console.log("result is ".concat(addingResultNominator / gcd, "/").concat(d / gcd));
     }
   },
-  // Метод для представления дроби в виде строки
-  toString: function toString() {
-    return "".concat(this.numerator, "/").concat(this.denominator);
+  subtraction: function subtraction(n, d) {
+    var localDenominator = 1;
+
+    if (this.denominator === d) {
+      localDenominator = this.denominator;
+    } else {
+      localDenominator = this.denominator * d;
+      this.nominator = this.nominator * (localDenominator / this.denominator);
+      this.denominator = localDenominator;
+      n = n * (localDenominator / d);
+      d = localDenominator;
+    }
   }
-}; // Пример использования объектов и методов
-
-var fraction1 = Fraction.create(1, 2);
-var fraction2 = Fraction.create(3, 4);
-var sum = fraction1.add(fraction2);
-console.log("Додвання:", sum.toString()); // Выведет "Сумма: 5/4"
-
-var difference = fraction1.subtract(fraction2);
-console.log("Віднімання:", difference.toString()); // Выведет "Разность: -1/4"
-
-var product = fraction1.multiply(fraction2);
-console.log("Множення:", product.toString()); // Выведет "Произведение: 3/8"
-
-var quotient = fraction1.divide(fraction2);
-console.log("Ділення:", quotient.toString()); // Выведет "Частное: 2/3"
-
-var reducedFraction = fraction1.reduce();
-console.log("Скрочений дріб:", reducedFraction.toString()); // Выведет "Сокращенная дробь: 1/2"
+};
--- 21 Об'єкти/JS/main.js
+++ 21 Об'єкти/JS/main.js
@@ -23,48 +23,48 @@ const car = {
   },
 
   consumption() {
-    const fix = (this.distance / 100) * this.fuel
-    console.log(fix);
+    const fixFuel = (this.distance / 100) * this.fuel
+    console.log(`всього потрібно ${fixFuel}л. пального`);
   },
 
   calcTime() {
     let hours = this.distance / this.averSreed
-    // console.log(hours);
+    if (hours >= 12) {
+      console.log(`час за який ви подолаєте відстань буде: ${hours + 3} год.`);      
+    }else if (hours >= 8) {
+      console.log(`час за який ви подолаєте відстань буде: ${hours + 2} год.`);
+    }else if (hours >= 4) {
+      console.log(`час за який ви подолаєте відстань буде: ${hours + 1} год.`);
        
-    if (hours >= 4) {
-      console.log(hours + 1);
-       
-    }else if (hours >= 9) {
-      console.log(hours + 2);
     } 
     
   }
-
-
+  
 }
+
+
+
 car.addDriver('Dima')
 console.log(car.checkDriver('Victor'));
 car.showInfo()
-car.distance = 2000
+car.distance = 3000
 car.consumption()
 car.calcTime()
 
 //normal
-
-const Time = {
+const time = {
   hours: 0,
   minutes: 0,
   seconds: 0,
 
   
   displayTime: function() {
-      const hoursStr = String(this.hours).padStart(2, "0");
-      const minutesStr = String(this.minutes).padStart(2, "0");
-      const secondsStr = String(this.seconds).padStart(2, "0");
+      const hoursStr = this.hours < 10 ? "0" + this.hours : this.hours.toString();
+      const minutesStr = this.minutes < 10 ? "0" + this.minutes : this.minutes.toString();
+      const secondsStr = this.seconds < 10 ? "0" + this.seconds : this.seconds.toString();
       console.log(`${hoursStr}:${minutesStr}:${secondsStr}`);
   },
-
-  // Функция для изменения времени на переданное количество секунд
+  
   addSeconds: function(seconds) {
       const totalSeconds = this.hours * 3600 + this.minutes * 60 + this.seconds + seconds;
       this.hours = Math.floor(totalSeconds / 3600) % 24;
@@ -73,152 +73,77 @@ const Time = {
       this.seconds = remainingSeconds % 60;
   },
 
-  // Функция для изменения времени на переданное количество минут
   addMinutes: function(minutes) {
       this.addSeconds(minutes * 60);
   },
-
-  // Функция для изменения времени на переданное количество часов
+ 
   addHours: function(hours) {
       this.addSeconds(hours * 3600);
   }
 };
 
-// Пример использования объекта и функций
-const currentTime = Object.create(Time);
-currentTime.hours = 20;
-currentTime.minutes = 59;
-currentTime.seconds = 45;
 
-currentTime.displayTime(); // Выведет "20:59:45"
+const currentTime = Object.create(time);
+// currentTime.hours = 20;
+// currentTime.minutes = 59;
+// currentTime.seconds = 45;
 
-currentTime.addSeconds(30);
-currentTime.displayTime(); // Выведет "21:00:15"
+currentTime.displayTime(); 
 
-currentTime.addMinutes(75);
-currentTime.displayTime(); // Выведет "22:15:15"
+currentTime.addSeconds(101);
+currentTime.displayTime(); 
 
-currentTime.addHours(2);
-currentTime.displayTime(); // Выведет "00:15:15" (следующий день)
+currentTime.addMinutes(250);
+currentTime.displayTime(); 
 
-// const time = {
-//   hours: 0,
-//   minute: 0,
-//   seconds: 0,
-// }
-
-
-// function enterTime() {  
-//   let now = new Date();
-//   console.log( now );
-// }
+currentTime.addHours(14);
+currentTime.displayTime(); 
 
 
 
-// function calcTimeSec() {
-//   let date = new Date();
-//   date.setSeconds(date.getSeconds() + 300);
-
-//   alert( date );
-
-
-// }
-
-// function calcTimeMin() {
-//   let date = new Date();
-//   date.setMinutes(date.getMinutes() + 300);
-
-//   alert( date );
-
-
-// }
-// function calcTimeHours() {
-//   let date = new Date();
-//   date.setHours(date.getHours() + 300);
-
-//   alert( date );
-
-
-// }
 //max
-
-
-const Fraction = {
-  numerator: 0,   
-  denominator: 1, 
-
-  // Метод для создания новой дроби
-  create: function(numerator, denominator) {
-      const fraction = Object.create(this);
-      fraction.numerator = numerator;
-      fraction.denominator = denominator;
-      return fraction;
-  },
-
-  // Метод для сложения двух дробей
-  add: function(otherFraction) {
-      const commonDenominator = this.denominator * otherFraction.denominator;
-      const newNumerator = this.numerator * otherFraction.denominator + otherFraction.numerator * this.denominator;
-      return Fraction.create(newNumerator, commonDenominator);
-  },
-
-  // Метод для вычитания двух дробей
-  subtract: function(otherFraction) {
-      const commonDenominator = this.denominator * otherFraction.denominator;
-      const newNumerator = this.numerator * otherFraction.denominator - otherFraction.numerator * this.denominator;
-      return Fraction.create(newNumerator, commonDenominator);
-  },
-
-  // Метод для умножения двух дробей
-  multiply: function(otherFraction) {
-      const newNumerator = this.numerator * otherFraction.numerator;
-      const newDenominator = this.denominator * otherFraction.denominator;
-      return Fraction.create(newNumerator, newDenominator);
-  },
-
-  // Метод для деления двух дробей
-  divide: function(otherFraction) {
-      const newNumerator = this.numerator * otherFraction.denominator;
-      const newDenominator = this.denominator * otherFraction.numerator;
-      return Fraction.create(newNumerator, newDenominator);
-  },
-
-  // Метод для сокращения дроби
-  reduce: function() {
-      const gcd = this.gcd(this.numerator, this.denominator);
-      return Fraction.create(this.numerator / gcd, this.denominator / gcd);
-  },
-
-  // Метод для нахождения наибольшего общего делителя (Алгоритм Евклида)
-  gcd: function(a, b) {
-      if (b === 0) {
-          return a;
-      } else {
-          return this.gcd(b, a % b);
-      }
-  },
-
-  // Метод для представления дроби в виде строки
-  toString: function() {
-      return `${this.numerator}/${this.denominator}`;
+const fraction = {
+nominator: 2,
+denominator: 3,
+showValue() {
+    console.log(`Value is ${this.nominator}/${this.denominator}`);
+},
+findGCD(a, b) {
+    if(a == 0) {
+        return b
+    }
+    return this.findGCD(b % a, a)
+},
+findLocalNominator(n, d) {
+    return n * d
+},
+add(n, d) {
+    let localDenominator = 1;
+    if(this.denominator === d) {
+        localDenominator = this.denominator
+    } else {
+        localDenominator = this.denominator * d
+        this.nominator = this.nominator * (localDenominator / this.denominator)
+        this.denominator = localDenominator;
+        n = n * (localDenominator / d);
+        d = localDenominator;
+        const addingResultNominator = this.nominator + n;
+        const gcd = this.findGCD(addingResultNominator, d);
+        console.log(`result is ${addingResultNominator / gcd}/${d / gcd}`);
+    }
+},
+subtraction(n, d){
+  let localDenominator = 1;
+  if (this.denominator === d) {
+    localDenominator = this.denominator
+  } else{
+    localDenominator = this.denominator * d
+    this.nominator = this.nominator * (localDenominator / this.denominator)
+    this.denominator = localDenominator;
+    n = n * (localDenominator / d);
+    d = localDenominator;
   }
-};
+}
+}
 
-// Пример использования объектов и методов
-const fraction1 = Fraction.create(1, 2);
-const fraction2 = Fraction.create(3, 4);
 
-const sum = fraction1.add(fraction2);
-console.log("Додвання:", sum.toString()); // Выведет "Сумма: 5/4"
-
-const difference = fraction1.subtract(fraction2);
-console.log("Віднімання:", difference.toString()); // Выведет "Разность: -1/4"
-
-const product = fraction1.multiply(fraction2);
-console.log("Множення:", product.toString()); // Выведет "Произведение: 3/8"
-
-const quotient = fraction1.divide(fraction2);
-console.log("Ділення:", quotient.toString()); // Выведет "Частное: 2/3"
-
-const reducedFraction = fraction1.reduce();
-console.log("Скрочений дріб:", reducedFraction.toString()); // Выведет "Сокращенная дробь: 1/2"
