2fc03dd40e2fd72fe5cf5d9bc93c065f8866fa66
KorolYurii <sminser111@gmail.com>
2023-10-09 19:43:20 +0300

    hw25

---
+++ 25/JS/main.js
@@ -0,0 +1,81 @@
+$(function () {
+    $('.slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+    $('.arrivals_slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: $('.arrivals_arrow__left'),
+        nextArrow: $('.arrivals_arrow__right'),
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings:{
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    autoplay: false,
+                }            
+            },
+            {
+                breakpoint: 768,
+                settings:{
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    autoplay: false,
+                }            
+            },
+            {
+                breakpoint: 500,
+                settings:{
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: false,
+                }            
+            },
+            
+             
+        ]
+    })
+    $('.partners_slider').slick({
+        slidesToShow: 9,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        prevArrow: $('.partners_arrow__left'),
+        nextArrow: $('.partners_arrow__right'),
+        responsive: [
+        {
+            breakpoint: 1100,
+            settings:{
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                autoplay: false,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings:{
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                autoplay: false,
+            }            
+        },
+        {
+            breakpoint: 500,
+            settings:{
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                autoplay: false,
+            }            
+        },
+             
+        ]
+    })
+})
+
+
+  
