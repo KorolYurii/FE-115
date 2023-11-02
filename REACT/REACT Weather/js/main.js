4e92be17508b0e2699f9b24e581b1f4a17eb778d
KorolYurii <sminser111@gmail.com>
2023-11-02 18:49:05 +0200

    error

--- REACT/REACT Weather/js/main.js
+++ REACT/REACT Weather/js/main.js
@@ -28,58 +28,58 @@ function HeaderSection(props) {
 
 
 
-function GridSection(props) {
-    return (
-        <div>
-            <div className="lisbon w-50">Lisbon  <br />
-                <span>21°C </span>
-                <i className="wi wi-day-sunny"></i>
-            </div>
-            <div className="paris w-25">Paris <br />
-                <span>11°C </span>
-                <i className="wi wi-night-sleet"></i>
-            </div>
-            <div className="belgrade w-25">Belgrade <br />
-                <span>15°C </span>
-                <i className="wi wi-day-cloudy"></i>
-            </div>
+// function GridSection(props) {
+//     return (
+//         <div>
+//             <div className="lisbon w-50">Lisbon  <br />
+//                 <span>21°C </span>
+//                 <i className="wi wi-day-sunny"></i>
+//             </div>
+//             <div className="paris w-25">Paris <br />
+//                 <span>11°C </span>
+//                 <i className="wi wi-night-sleet"></i>
+//             </div>
+//             <div className="belgrade w-25">Belgrade <br />
+//                 <span>15°C </span>
+//                 <i className="wi wi-day-cloudy"></i>
+//             </div>
 
-            <div className="venice w-25">Venice <br />
-                <span>21°C </span>
-                <i className="wi wi-day-cloudy-high"></i>
-            </div>
-            <div className="tel-avive w-25">Tel-Avive <br />
-                <span>32°C </span>
-                <i className="wi wi-hot"></i>
-            </div>
-            <div className="cair w-25">Cair <br />
-                <span>21°C </span>
-                <i className="wi wi-day-sunny"></i>
-            </div>
-            <div className="new-york w-25">New-York <br />
-                <span>17°C </span>                    
-                <i className="wi wi-day-sleet-storm"></i>
-            </div>
+//             <div className="venice w-25">Venice <br />
+//                 <span>21°C </span>
+//                 <i className="wi wi-day-cloudy-high"></i>
+//             </div>
+//             <div className="tel-avive w-25">Tel-Avive <br />
+//                 <span>32°C </span>
+//                 <i className="wi wi-hot"></i>
+//             </div>
+//             <div className="cair w-25">Cair <br />
+//                 <span>21°C </span>
+//                 <i className="wi wi-day-sunny"></i>
+//             </div>
+//             <div className="new-york w-25">New-York <br />
+//                 <span>17°C </span>                    
+//                 <i className="wi wi-day-sleet-storm"></i>
+//             </div>
 
-            <div className="new-delhi w-25">New-Delhi <br />
-                <span>17°C </span>
-                <i className="wi wi-rain-wind"></i>
-            </div>
-            <div className="san-francisco w-50"> San-Francisco <br />
-                <span>15°C </span>
-                <i className="wi wi-day-cloudy"></i>
-            </div>
-            <div className="tokyo w-25">Tokyo <br />
-                <span>8°C </span>
-                <i className="wi wi-night-clear"></i>
-            </div>
-            <div className="sydney w-100">Sydney <br />
-                <span>25°C </span>
-                <i className="wi wi-night-partly-cloudy"></i>
-            </div>
-        </div>
-    )
-}
+//             <div className="new-delhi w-25">New-Delhi <br />
+//                 <span>17°C </span>
+//                 <i className="wi wi-rain-wind"></i>
+//             </div>
+//             <div className="san-francisco w-50"> San-Francisco <br />
+//                 <span>15°C </span>
+//                 <i className="wi wi-day-cloudy"></i>
+//             </div>
+//             <div className="tokyo w-25">Tokyo <br />
+//                 <span>8°C </span>
+//                 <i className="wi wi-night-clear"></i>
+//             </div>
+//             <div className="sydney w-100">Sydney <br />
+//                 <span>25°C </span>
+//                 <i className="wi wi-night-partly-cloudy"></i>
+//             </div>
+//         </div>
+//     )
+// }
 function TextSection(props) {
     return (
         <p className="text">Have a nice day and don't forget umbrella if you are in New Delhi now!</p>
