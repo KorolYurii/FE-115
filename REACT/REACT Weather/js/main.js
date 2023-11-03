9d9244b374c90dc1cc8c6ffbca2c2c2b06c6430c
KorolYurii <sminser111@gmail.com>
2023-11-03 14:31:12 +0200

    weather

--- REACT/REACT Weather/js/main.js
+++ REACT/REACT Weather/js/main.js
@@ -6,17 +6,12 @@ function App(props) {
         <section className="section">
             <div className="container">
                 <HeaderSection/>
-                <div className='grid'>
-                    <GridItem/>
-                </div>
-                
+                <GridItem/>                
                 <TextSection/>
             </div>
         </section>
     )
 }
-
-
 
 function HeaderSection(props) {
     return (
@@ -25,6 +20,131 @@ function HeaderSection(props) {
         </h1>
     )
 }
+
+function TextSection(props) {
+    return (
+        <p className="text">Have a nice day and don't forget umbrella if you are in New Delhi now!</p>
+    )
+}
+
+
+function GridItem(props) {
+    let weatherItem;
+    if(weatherList.length) {
+        weatherItem = weatherList.map((card, city) => {
+            return <GridWeatherItem item={card} key={city}/>
+        })
+    }
+    
+    
+
+    return (
+        <div className="grid">
+            {weatherItem}
+        </div>
+    )
+}
+
+function GridWeatherItem(props) {
+    const { city, temp, icon, width, color } = props.item;
+    const classIcon = `wi ${icon}`;
+
+    return (
+        <div className={width} style={{backgroundColor: color}}>
+            <p className="city_name">{city}</p>
+            <span className="card_temp">{temp}°C; </span>
+            <i className={classIcon}></i>        
+      </div> 
+    )
+    
+}
+
+const weatherList = [
+    {
+        city: 'Lisbon',
+        temp: '21',
+        icon: 'wi wi-day-sunny',
+        width: 'w-50',
+        color: '#38b4e9'
+    },
+    {
+        city: 'Paris',
+        temp: '11',
+        icon: 'wi-night-sleet',
+        width: 'w-25',
+        color: '#3CC'
+    },
+    {
+        city: 'Belgrade',
+        temp: '15',
+        icon: 'wi-day-cloudy',
+        width: 'w-25',
+        color: '#969'
+    },
+    {
+        city: 'Venice',
+        temp: '21',
+        icon: 'wi-day-cloudy-high',
+        width: 'w-25',
+        color: '#C24747'
+    },
+    {
+        city: 'Tel-Avive',
+        temp: '32',
+        icon: 'wi-hot',
+        width: 'w-25',
+        color: '#E2674A'
+    },
+    {
+        city: 'Cair',
+        temp: '21',
+        icon: 'wi-day-sunny',
+        width: 'w-25',
+        color: '#FC6'
+    },
+    {
+        city: 'New-York',
+        temp: '17',
+        icon: 'wi-day-sleet-storm',
+        width: 'w-25',
+        color: '#9C9'
+    },
+    {
+        city: 'New-Delhi',
+        temp: '17',
+        icon: 'wi-rain-wind',
+        width: 'w-25',
+        color: '#699'
+    },
+    {
+        city: 'San-Francisco',
+        temp: '15',
+        icon: 'wi-day-cloudy',
+        width: 'w-50',
+        color: '#C69'
+    },
+    {
+        city: 'Tokyo',
+        temp: '8',
+        icon: 'wi-night-clear',
+        width: 'w-25',
+        color: '#396'
+    },
+    {
+        city: 'Sydney',
+        temp: '25',
+        icon: 'wi-night-partly-cloudy',
+        width: 'w-100',
+        color: '#669'
+    }
+
+]
+
+
+const app = document.getElementById('app');
+const root = ReactDOM.createRoot(app);
+root.render(React.createElement(App))
+
 
 
 
@@ -80,101 +200,3 @@ function HeaderSection(props) {
 //         </div>
 //     )
 // }
-function TextSection(props) {
-    return (
-        <p className="text">Have a nice day and don't forget umbrella if you are in New Delhi now!</p>
-    )
-}
-
-
-function GridItem(props) {
-    let weatherItem;
-    if(weatherList.length) {
-        weatherItem = weatherList.map((card, city) => {
-            return <GridItem item={card}  key={city}/>
-        })
-    }
-    
-    console.log(weatherItem);
-
-    return (
-        <div className="grid">Sydney <br />
-            {weatherItem}
-        </div>
-    )
-}
-
-const weatherList = [
-    {
-        city: 'Lisbon',
-        temp: '21',
-        icon: 'icon-sun',
-        width: 50
-    },
-    {
-        city: 'Paris',
-        temp: '11',
-        icon: 'icon-sun',
-        width: 25
-    },
-    {
-        city: 'Belgrade',
-        temp: '15',
-        icon: 'icon-sun',
-        width: 50
-    },
-    {
-        city: 'Venice',
-        temp: '21',
-        icon: 'icon-sun',
-        width: 25
-    },
-    {
-        city: 'Tel-Avive',
-        temp: '32',
-        icon: 'icon-sun',
-        width: 25
-    },
-    {
-        city: 'Cair',
-        temp: '21',
-        icon: 'icon-sun',
-        width: 25
-    },
-    {
-        city: 'New-York',
-        temp: '17',
-        icon: 'icon-sun',
-        width: 25
-    },
-    {
-        city: 'New-Delhi',
-        temp: '17',
-        icon: 'icon-sun',
-        width: 25
-    },
-    {
-        city: 'San-Francisco',
-        temp: '15',
-        icon: 'icon-sun',
-        width: 50
-    },
-    {
-        city: 'Tokyo',
-        temp: '8',
-        icon: 'icon-sun',
-        width: 25
-    },
-    {
-        city: 'Sydney',
-        temp: '25',
-        icon: 'icon-sun',
-        width: 100
-    }
-
-]
-
-
-const app = document.getElementById('app');
-const root = ReactDOM.createRoot(app);
-root.render(React.createElement(App))
