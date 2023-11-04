abeaf794f656a42ab24ef8a90ab0d7d8c6c0daed
KorolYurii <sminser111@gmail.com>
2023-11-04 10:01:13 +0200

    weather

--- REACT/REACT Weather/js/main.js
+++ REACT/REACT Weather/js/main.js
@@ -12,11 +12,7 @@ function App() {
     )
 }
 
-<<<<<<< HEAD
 function HeaderSection() {
-=======
-function HeaderSection(props) {
->>>>>>> 9d9244b374c90dc1cc8c6ffbca2c2c2b06c6430c
     return (
         <h1 className="header">CSS Weather Forecast
             <i className="wi wi-sunset"></i>
@@ -24,22 +20,13 @@ function HeaderSection(props) {
     )
 }
 
-<<<<<<< HEAD
 function TextSection() {
-=======
-function TextSection(props) {
->>>>>>> 9d9244b374c90dc1cc8c6ffbca2c2c2b06c6430c
     return (
         <p className="text">Have a nice day and don't forget umbrella if you are in New Delhi now!</p>
     )
 }
 
-<<<<<<< HEAD
 function GridItem() {
-=======
-
-function GridItem(props) {
->>>>>>> 9d9244b374c90dc1cc8c6ffbca2c2c2b06c6430c
     let weatherItem;
     if(weatherList.length) {
         weatherItem = weatherList.map((card, city) => {
@@ -47,11 +34,6 @@ function GridItem(props) {
         })
     }
     
-<<<<<<< HEAD
-=======
-    
-
->>>>>>> 9d9244b374c90dc1cc8c6ffbca2c2c2b06c6430c
     return (
         <div className="grid">
             {weatherItem}
@@ -65,13 +47,8 @@ function GridWeatherItem(props) {
 
     return (
         <div className={width} style={{backgroundColor: color}}>
-<<<<<<< HEAD
             <p>{city}</p>
             <span>{temp}°C; </span>
-=======
-            <p className="city_name">{city}</p>
-            <span className="card_temp">{temp}°C; </span>
->>>>>>> 9d9244b374c90dc1cc8c6ffbca2c2c2b06c6430c
             <i className={classIcon}></i>        
       </div> 
     )
@@ -82,11 +59,7 @@ const weatherList = [
     {
         city: 'Lisbon',
         temp: '21',
-<<<<<<< HEAD
         icon: 'wi-day-sunny',
-=======
-        icon: 'wi wi-day-sunny',
->>>>>>> 9d9244b374c90dc1cc8c6ffbca2c2c2b06c6430c
         width: 'w-50',
         color: '#38b4e9'
     },
@@ -171,58 +144,3 @@ root.render(React.createElement(App))
 
 
 
-<<<<<<< HEAD
-=======
-// function GridSection(props) {
-//     return (
-//         <div>
-//             <div className="lisbon w-50">Lisbon  <br />
-//                 <span>21°C </span>
-//                 <i className="wi wi-day-sunny"></i>
-//             </div>
-//             <div className="paris w-25">Paris <br />
-//                 <span>11°C </span>
-//                 <i className="wi wi-night-sleet"></i>
-//             </div>
-//             <div className="belgrade w-25">Belgrade <br />
-//                 <span>15°C </span>
-//                 <i className="wi wi-day-cloudy"></i>
-//             </div>
-
-//             <div className="venice w-25">Venice <br />
-//                 <span>21°C </span>
-//                 <i className="wi wi-day-cloudy-high"></i>
-//             </div>
-//             <div className="tel-avive w-25">Tel-Avive <br />
-//                 <span>32°C </span>
-//                 <i className="wi wi-hot"></i>
-//             </div>
-//             <div className="cair w-25">Cair <br />
-//                 <span>21°C </span>
-//                 <i className="wi wi-day-sunny"></i>
-//             </div>
-//             <div className="new-york w-25">New-York <br />
-//                 <span>17°C </span>                    
-//                 <i className="wi wi-day-sleet-storm"></i>
-//             </div>
-
-//             <div className="new-delhi w-25">New-Delhi <br />
-//                 <span>17°C </span>
-//                 <i className="wi wi-rain-wind"></i>
-//             </div>
-//             <div className="san-francisco w-50"> San-Francisco <br />
-//                 <span>15°C </span>
-//                 <i className="wi wi-day-cloudy"></i>
-//             </div>
-//             <div className="tokyo w-25">Tokyo <br />
-//                 <span>8°C </span>
-//                 <i className="wi wi-night-clear"></i>
-//             </div>
-//             <div className="sydney w-100">Sydney <br />
-//                 <span>25°C </span>
-//                 <i className="wi wi-night-partly-cloudy"></i>
-//             </div>
-//         </div>
-//     )
-// }
->>>>>>> 9d9244b374c90dc1cc8c6ffbca2c2c2b06c6430c
