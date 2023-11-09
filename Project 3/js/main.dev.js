94ae97b0f54e73382b18e724b5a1e6a32136ac04
KorolYurii <sminser111@gmail.com>
2023-11-09 15:48:29 +0200

    project 3

--- Project 3/js/main.dev.js
+++ Project 3/js/main.dev.js
@@ -1,50 +1,53 @@
 "use strict";
 
-var API_KEY = 'af4b1ca7e93f744d85867d9d2419981f';
-var BASE_URL = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=".concat(API_KEY); //`https://api.musixmatch.com/ws/1.1/track.get?apikey=${API_KEY}`;
+var API_KEY = '4c353af824d786d3afe3b55e6a4178eb';
+var BASE_URL = "http://api.openweathermap.org/data/2.5/weather?units=metric&appid=".concat(API_KEY); //const GEO_URL = `http://api.openweathermap.org/geo/1.0/direct?&appid=${API_KEY}`;
 
-var PAGE = 1;
-var finalPage = 1; // http://api.musixmatch.com/ws/1.1/track.search?q_artist=justin bieber&page_size=3&page=1&s_track_rating=desc
+var DAYS_URL = "https://api.openweathermap.org/data/2.5/forecast?&appid=".concat(API_KEY, "&units=metric");
 
-function searchMusic() {
-  var musicName = document.getElementById('search').value;
+function searchCity() {
+  var cityName = document.getElementById('search').value;
 
-  if (musicName) {
-    //console.log(musicName);
-    axios.get(BASE_URL + "&q=".concat(musicName)).then(function (res) {
-      console.log(res.data);
-      finalPage = Math.ceil(res.data.totalResults / 10);
-      console.log(finalPage);
-      generateCards(res.data.Search);
+  if (cityName) {
+    console.log(cityName);
+    axios.get(BASE_URL + "&q=".concat(cityName)).then(function (res) {
+      console.log(res);
+      generateCards(res.data);
     });
   }
 }
 
-function nextPage(event) {
-  event.preventDefault();
-  PAGE++;
-  gotoPage(PAGE);
+function generateCards(results) {
+  var ICON_URL = "https://openweathermap.org/img/wn/".concat(results.weather[0].icon, "@2x.png");
+  var html = "<div class=\"list\">";
+  html = html + "\n    <span class=\"country\">".concat(results.sys.country, ",</span>\n    <span class=\"city_name\">").concat(results.name, "</span> </br> \n    <span class=\"description\">").concat(results.weather[0].description, "</span> </br>    \n    <img src=\"").concat(ICON_URL, "\" class=\"icon\"></img> </br>\n    <span class=\"temp\">").concat(results.main.temp, " \xB0C</span> </br>         \n    <span class=\"speed\">wind speed:  ").concat(results.wind.speed, " m/s</span>    \n    ");
+  html = html + '</div>';
+  document.getElementById('content').innerHTML = html;
 }
 
-function gotoPage(page) {
-  PAGE = page;
-  searchMovie();
+function searchDaysCity() {
+  var cityName = document.getElementById('search').value;
+
+  if (cityName) {
+    console.log(cityName);
+    axios.get(DAYS_URL + "&q=".concat(cityName)).then(function (res) {
+      console.log(res.data); // const lat = res.data[0].lat;
+      // const lon = res.data[0].lon;            
+      //const days_url = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+      // const days_url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+
+      generateDaysCards(res.data);
+    });
+  }
 }
 
-function goToEnd(event) {
-  event.preventDefault();
-  PAGE = finalPage;
-  gotoPage(PAGE);
-}
-
-function prevPage(event) {
-  event.preventDefault();
-  PAGE--;
-  gotoPage(PAGE);
-}
-
-function goToStart(event) {
-  event.preventDefault();
-  PAGE = 1;
-  gotoPage(PAGE);
+function generateDaysCards(resultData) {
+  //const ICON_DAY_URL = `https://openweathermap.org/img/wn/${resultData.weather[0].icon}@2x.png`
+  var html = "<ul class=\"day_list\">";
+  resultData.list.forEach(function (el) {
+    var ICON_DAY_URL = "https://openweathermap.org/img/wn/".concat(el.weather[0].icon, "@2x.png");
+    html = html + "\n        <li class='day_list_item'>\n            <span class=\"txt\">".concat(el.dt_txt, ",</span></br> \n            <span class=\"temp\">").concat(el.main.temp, " \xB0C</span></br> \n            <img src=\"").concat(ICON_DAY_URL, "\" ></br\n            <span class=\"description\">").concat(el.weather[0].description, "</span></br>   \n            <span class=\"wind\">").concat(el.wind.speed, " m/s</span></br>\n        </li>");
+  });
+  html = html + '</ul>';
+  document.getElementById('day_content').innerHTML = html;
 }
--- Project 3/js/main.js
+++ Project 3/js/main.js
@@ -1,36 +1,85 @@
 const API_KEY = '4c353af824d786d3afe3b55e6a4178eb';
-const BASE_URL = `http://api.openweathermap.org/data/2.5/weather?units=metric&appid=${API_KEY}`
+const BASE_URL = `http://api.openweathermap.org/data/2.5/weather?units=metric&appid=${API_KEY}`;
 
+//const GEO_URL = `http://api.openweathermap.org/geo/1.0/direct?&appid=${API_KEY}`;
+const DAYS_URL = `https://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}&units=metric`
 
 function searchCity() {
     
     const cityName = document.getElementById('search').value;
+    
     if(cityName){
-       //console.log(cityName);
+       console.log(cityName);
         axios.get(BASE_URL+`&q=${cityName}`)
         .then(function (res) {
-            console.log(res.data);
-            
-            generateCards(res.data);
-            
+            console.log(res);            
+            generateCards(res.data);            
         })
     }  
-         
+           
 }
 
 
-function generateCards(resultsData){
+function generateCards(results){
+    const ICON_URL = `https://openweathermap.org/img/wn/${results.weather[0].icon}@2x.png`
 
-    let html = `<ul class="grid-list">`
+    let html = `<div class="list">`
+
+    html = html + `
+    <span class="country">${results.sys.country},</span>
+    <span class="city_name">${results.name}</span> </br> 
+    <span class="description">${results.weather[0].description}</span> </br>    
+    <img src="${ICON_URL}" class="icon"></img> </br>
+    <span class="temp">${results.main.temp} °C</span> </br>         
+    <span class="speed">wind speed:  ${results.wind.speed} m/s</span>    
+    `
     
-    resultsData.forEach(el => {
-        html = html + `<li class="card_item">
-        <span>${el.weather.icon}</span>
-        <span>${el.main.feels_like}</span>                
+    html = html + '</div>'
+    document.getElementById('content').innerHTML = html;    
+}
+
+function searchDaysCity() {
+    
+    const cityName = document.getElementById('search').value;
+
+    if(cityName){
+       console.log(cityName);
+        axios.get(DAYS_URL+`&q=${cityName}`)
+        .then(function (res) {
+            console.log(res.data);
+            // const lat = res.data[0].lat;
+            // const lon = res.data[0].lon;            
+            //const days_url = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+            // const days_url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+            
+               
+            generateDaysCards(res.data)                     
+        })        
+    }           
+}
+
+function generateDaysCards(resultData){
+    //const ICON_DAY_URL = `https://openweathermap.org/img/wn/${resultData.weather[0].icon}@2x.png`
+    
+    let html = `<ul class="day_list">`
+
+       
+    resultData.list.forEach(el => {
+        const ICON_DAY_URL = `https://openweathermap.org/img/wn/${el.weather[0].icon}@2x.png`
+        html = html + `
+        <li class='day_list_item'>
+            <span class="txt">${el.dt_txt},</span></br> 
+            <span class="temp">${el.main.temp} °C</span></br> 
+            <img src="${ICON_DAY_URL}" ></br
+            <span class="description">${el.weather[0].description}</span></br>   
+            <span class="wind">${el.wind.speed} m/s</span></br>
         </li>`
-    });
-    html = html + '</ul>'
-    document.getElementById('content').innerHTML = html;
+        
+        
+    })
     
+    
+    
+    html = html + '</ul>'
+    document.getElementById('day_content').innerHTML = html;    
 }
-
